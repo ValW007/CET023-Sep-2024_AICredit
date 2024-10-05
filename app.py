46cb69865d7d9841a48921755545511c31d453a3
ValW007 <k2420737c@e.ntu.edu.sg>
2024-10-05 05:03:37 +0000

    1

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request #flask is a web framework for Python that allows one to build web application.
 #Create model for faq
 import google.generativeai as genai
 
 model = genai.GenerativeModel ("gemini-1.5-flash")
 genai.configure(api_key= "AIzaSyCFAOkFd5qjen2xJmfrQjbYAq4pb9oz7fI")
 
-app = Flask(__name__)
-@app.route("/",methods=["GET","POST"])
+app = Flask(__name__) #create an instance of the web framework for python, where name = root path of the application
+@app.route("/",methods=["GET","POST"]) #use flask to map to root URL endpoint denoted by "/". #GET = retrieve from server when URL is accessed, #POST = submit a form
 #GET from SG to US, and POST is US to SG
-def index():
-        return(render_template("index.html"))
+def index(): #when "/" is called, generate function called index
+        return(render_template("index.html")) # Flask function will look to index.html under templates for the generation
 
-@app.route("/prediction_DBS",methods=["GET","POST"])
-def prediction_DBS():
-        return(render_template("prediction_DBS.html"))
+@app.route("/prediction_DBS",methods=["GET","POST"]) 
+def prediction_DBS(): #when "/prediction_DBS" function is called
+        return(render_template("prediction_DBS.html")) #Flask function will look to "prediction_DBS.html" under templates to generate
 
 #Setting up the Results Page
 @app.route("/prediction_result_DBS",methods=["GET","POST"])
@@ -38,6 +38,18 @@ def q2():
         q=request.form.get("q")
         r= model.generate_content(q)
         return(render_template("q2_reply.html",r=r)) #synchronise front and back-end.
+
+#Function to run the Button at the Front End
+@app.route("/predictcreditability",methods=["GET","POST"])
+def predictcreditability():
+        return(render_template("predictcreditability.html")) #synchronise front and back-end.
+
+#Setting up the Predict Credibility Result Page
+@app.route("/predictcreditability_result",methods=["GET","POST"])
+def predictcreditability_result():
+        q=float(request.form.get("q")) # wsgi is text by default, therefore to force to float, where q is the variable
+        r=(-0.00011807*q)+1.20396988
+        return(render_template("predictcreditability_result.html",r=r)) #synchronise front and back-end.
           
 #Run in the cloud. #__name__ is to confirm that this is the application to run.
 if __name__=="__main__":
